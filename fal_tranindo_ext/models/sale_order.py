81aa1f67e79cb69ab25fc63ee4d298f9edb2d20c
thq2307 <achmad.thoriq@falinwa.com>
2022-10-21 10:16:18 +0700

    (Oct 20th 2022)[UPD]Turn off the onchange that change salesperson(user_id) when changing or adding customer(partner_id)

--- fal_tranindo_ext/models/sale_order.py
+++ fal_tranindo_ext/models/sale_order.py
@@ -1,5 +1,6 @@
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_is_zero, html_keep_url, is_html_empty
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -18,6 +19,12 @@ class SaleOrder(models.Model):
 
     invoice_street = fields.Many2one("res.partner", string="Invoice Address")
     delivery_street = fields.Many2one("res.partner", string="Delivery Address")
+
+    user_id = fields.Many2one(
+        'res.users', string='Salesperson', index=True, tracking=2, default=lambda self: self.env.user,
+        domain=lambda self: "[('groups_id', '=', {}), ('share', '=', False), ('company_ids', '=', company_id)]".format(
+            self.env.ref("sales_team.group_sale_salesman").id
+        ),)
 
     @api.onchange('final_customer')
     def _onchange_delivery_invoice(self):
@@ -83,3 +90,50 @@ class SaleOrder(models.Model):
     def _check_duplicate_customer_reference(self, cust_ref, company):
         if self.search([('client_order_ref', '=', cust_ref), ('company_id', '=', company)]):
             raise UserError(_("Duplicated Customer Reference detected. You probably encoded twice the same Quotation."))
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        """
+        Update the following fields when the partner is changed:
+        - Pricelist
+        - Payment terms
+        - Invoice address
+        - Delivery address
+        - Sales Team
+        """
+        if not self.partner_id:
+            self.update({
+                'partner_invoice_id': False,
+                'partner_shipping_id': False,
+                'fiscal_position_id': False,
+            })
+            return
+
+        self = self.with_company(self.company_id)
+
+        addr = self.partner_id.address_get(['delivery', 'invoice'])
+        # partner_user = self.partner_id.user_id or self.partner_id.commercial_partner_id.user_id
+        values = {
+            'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
+            'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
+            'partner_invoice_id': addr['invoice'],
+            'partner_shipping_id': addr['delivery'],
+        }
+        # user_id = partner_user.id
+        # if not self.env.context.get('not_self_saleperson'):
+        #     user_id = user_id or self.env.context.get('default_user_id', self.env.uid)
+        # if user_id and self.user_id.id != user_id:
+        #     values['user_id'] = user_id
+
+        if self.env['ir.config_parameter'].sudo().get_param('account.use_invoice_terms'):
+            if self.terms_type == 'html' and self.env.company.invoice_terms_html:
+                baseurl = html_keep_url(self.get_base_url() + '/terms')
+                values['note'] = _('Terms & Conditions: %s', baseurl)
+            elif not is_html_empty(self.env.company.invoice_terms):
+                values['note'] = self.with_context(lang=self.partner_id.lang).env.company.invoice_terms
+        if not self.env.context.get('not_self_saleperson') or not self.team_id:
+            default_team = self.env.context.get('default_team_id', False) or self.partner_id.team_id.id
+            values['team_id'] = self.env['crm.team'].with_context(
+                default_team_id=default_team
+            )._get_default_team_id(domain=['|', ('company_id', '=', self.company_id.id), ('company_id', '=', False)])
+        self.update(values)
