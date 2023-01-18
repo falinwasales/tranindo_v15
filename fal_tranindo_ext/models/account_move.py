6c8a1c1ca6d88f676c7427258ff7eceb377c90c3
thq2307 <achmad.thoriq@falinwa.com>
2023-01-18 13:39:37 +0700

    TEST

--- fal_tranindo_ext/models/account_move.py
+++ fal_tranindo_ext/models/account_move.py
@@ -48,6 +48,15 @@ class AccountInvoice(models.Model):
 
     payment_memo = fields.Char(string="Payment Memo")
 
+    category_first_line = fields.Many2one('product.category', string="Product Categoryx", store=True)
+
+    def get_first_line_category(self):
+        for record in self:
+            record.category_first_line = False
+            if record.move_type == 'out_invoice' and record.invoice_line_ids:
+                for line in record.invoice_line_ids:
+                    record.category_first_line = line[0].product_id.categ_id.id
+
     # def _get_memo_from_payment(self):
     #     for record in self:
     #         record.payment_memo = ''
