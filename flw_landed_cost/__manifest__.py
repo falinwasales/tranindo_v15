a1aabdf7e45510ad839c068b32fa10be8fadaf22
thq2307 <achmad.thoriq@falinwa.com>
2023-03-14 13:47:45 +0700

    UPDATE IN STOCK LANDED COSTS

---
+++ flw_landed_cost/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Flw Landed Cost",
+    'version': '15.0.1.0.0',
+    'license': 'OPL-1',
+    'summary': "Flw Landed Cost.",
+    "category": 'Landed Cost',
+    'author': "Falinwa",
+    'website': "https://www.cluedoo.com",
+    'support': 'cluedoo@falinwa.com',
+    "description": """
+        Landed Cost Custom Modules
+    """,
+    "depends": [
+        "account","stock","stock_landed_costs"
+    ],
+    "data": [
+        'views/stock_landed_cost_views.xml',
+    ],
+    'images': [
+    ],
+    'demo': [
+    ],
+    'price': 15,
+    'currency': 'EUR',
+    'application': False,
+}
